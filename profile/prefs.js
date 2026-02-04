debe7de22a6ab14fffe84c9d4664c22dff87c3af
gustavonline <8681157+gustavonline@users.noreply.github.com>
2026-02-04 10:16:17 +0100

    Auto-Sync (Live): 2/4/2026, 10:16:16 AM

--- profile/prefs.js
+++ profile/prefs.js
@@ -227,7 +227,7 @@ user_pref("services.sync.engine.prefs.modified", false);
 user_pref("services.sync.engine.workspaces", true);
 user_pref("services.sync.forms.lastSync", "0");
 user_pref("services.sync.forms.syncID", "gXsCOhObik_8");
-user_pref("services.sync.globalScore", 47);
+user_pref("services.sync.globalScore", 62);
 user_pref("services.sync.lastPing", 1770124524);
 user_pref("services.sync.lastSync", "Wed Feb 04 2026 10:11:26 GMT+0100 (Central European Standard Time)");
 user_pref("services.sync.lastTabFetch", 1770196286);
