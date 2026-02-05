5809970d2b3976a9a686a886d5456e0def869342
gustavonline <8681157+gustavonline@users.noreply.github.com>
2026-02-05 13:40:13 +0100

    Auto-Sync (Live): 2/5/2026, 1:40:13 PM

--- profile/prefs.js
+++ profile/prefs.js
@@ -244,7 +244,7 @@ user_pref("services.sync.engine.prefs.modified", false);
 user_pref("services.sync.engine.workspaces", true);
 user_pref("services.sync.forms.lastSync", "0");
 user_pref("services.sync.forms.syncID", "gXsCOhObik_8");
-user_pref("services.sync.globalScore", 7);
+user_pref("services.sync.globalScore", 17);
 user_pref("services.sync.lastPing", 1770211474);
 user_pref("services.sync.lastSync", "Thu Feb 05 2026 13:33:58 GMT+0100 (Central European Standard Time)");
 user_pref("services.sync.lastTabFetch", 1770294838);
@@ -285,5 +285,5 @@ user_pref("zen.urlbar.suggestions-learner", "{\"zen:global-action-remove-from-es
 user_pref("zen.view.compact.enable-at-startup", false);
 user_pref("zen.view.use-single-toolbar", false);
 user_pref("zen.welcome-screen.seen", true);
-user_pref("zen.workspaces.active", "{5ba09051-99a7-43e0-b598-7b3a24a72293}");
+user_pref("zen.workspaces.active", "{da1bc049-bb62-4b51-916d-dc91274a67b3}");
 user_pref("zen.workspaces.continue-where-left-off", true);
