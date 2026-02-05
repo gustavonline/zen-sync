30d81cbcdcd7cc49dc3333b386f6351da31e3d97
gustavonline <hello@gustavonline.com>
2026-02-05 20:07:41 +0100

    Auto-Sync (Live): 05/02/2026, 20.07.40

--- profile/prefs.js
+++ profile/prefs.js
@@ -239,7 +239,7 @@ user_pref("services.sync.engine.prefs.modified", false);
 user_pref("services.sync.engine.workspaces", true);
 user_pref("services.sync.forms.lastSync", "0");
 user_pref("services.sync.forms.syncID", "gXsCOhObik_8");
-user_pref("services.sync.globalScore", 4);
+user_pref("services.sync.globalScore", 5);
 user_pref("services.sync.lastPing", 1770297992);
 user_pref("services.sync.lastSync", "Thu Feb 05 2026 19:59:44 GMT+0100 (Central European Standard Time)");
 user_pref("services.sync.lastTabFetch", 1770317983);
