641212b1a2392acba40bea813c47f0b4eadbcd63
gustavonline <hello@gustavonline.com>
2026-02-06 06:46:32 +0100

    Auto-Sync (Live): 06/02/2026, 06.46.32

--- profile/prefs.js
+++ profile/prefs.js
@@ -164,7 +164,7 @@ user_pref("sanity-test.version", "20260203092256");
 user_pref("security.webauthn.show_ms_settings_link", true);
 user_pref("services.settings.blocklists.addons-bloomfilters.last_check", 1770345208);
 user_pref("services.settings.blocklists.gfx.last_check", 1770345208);
-user_pref("services.settings.clock_skew_seconds", 718);
+user_pref("services.settings.clock_skew_seconds", 1924);
 user_pref("services.settings.last_etag", "\"1770350232084\"");
 user_pref("services.settings.last_update_seconds", 1770354822);
 user_pref("services.settings.main.addons-data-leak-blocker-domains.last_check", 1770345208);
