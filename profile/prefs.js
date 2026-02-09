249633d46c59f6d9b3412fe411e0452145a034bf
gustavonline <hello@gustavonline.com>
2026-02-09 12:06:33 +0100

    Auto-Sync (Live): 09/02/2026, 12.06.33

--- profile/prefs.js
+++ profile/prefs.js
@@ -247,7 +247,7 @@ user_pref("services.sync.engine.prefs.modified", false);
 user_pref("services.sync.engine.workspaces", true);
 user_pref("services.sync.forms.lastSync", "0");
 user_pref("services.sync.forms.syncID", "gXsCOhObik_8");
-user_pref("services.sync.globalScore", 17);
+user_pref("services.sync.globalScore", 18);
 user_pref("services.sync.lastPing", 1770577609);
 user_pref("services.sync.lastSync", "Mon Feb 09 2026 12:01:37 GMT+0100 (Central European Standard Time)");
 user_pref("services.sync.lastSyncReassigned", true);
@@ -289,5 +289,5 @@ user_pref("zen.urlbar.suggestions-learner", "{\"zen:global-action-remove-from-es
 user_pref("zen.view.compact.enable-at-startup", false);
 user_pref("zen.view.use-single-toolbar", false);
 user_pref("zen.welcome-screen.seen", true);
-user_pref("zen.workspaces.active", "{6d45e9da-3aa2-4560-aeea-2b56820a2117}");
+user_pref("zen.workspaces.active", "{da1bc049-bb62-4b51-916d-dc91274a67b3}");
 user_pref("zen.workspaces.continue-where-left-off", true);
